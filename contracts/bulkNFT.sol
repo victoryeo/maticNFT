1d4863eb797c8109804a120095876840905405b3
yeosiowvic <victoryeo1974@gmail.com>
2021-12-04 21:40:30 +0800

    add bulk erc721 minting contract

---
+++ contracts/bulkNFT.sol
@@ -0,0 +1,59 @@
+// Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721]'
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract bulkNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    uint constant NUM = 2;
+    address[] recipiency;
+
+    constructor() ERC721("maticNFT", "NFT") {
+      // hardcoded address for testing
+      recipiency.push(0xE0f5206BBD039e7b0592d8918820024e2a7437b9);
+      recipiency.push(0x9106BcAFb5cdcbbA5bD0d98fBbf2d82fD4245201);
+      // call erc721 _mint
+      // _mint emits an event each time
+      for (uint i = 0; i < NUM; i++) {
+        super._mint(recipiency[i], i);
+      }
+    }
+
+    function registerAddress(address added) public { 
+      recipiency.push(added);  
+    }
+
+    //get the current supply of tokens
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    // for opensea collection 
+    function contractURI() public pure returns (string memory) {
+        return "https://ipfs.io/ipfs/QmcxJNpGFmAfxVwh56ik8v7DFHxRHCm6m1QfZGt3wKsWuW";
+    }
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function updateNFT(uint256 tokenId, string memory tokenURI)
+        public onlyOwner
+    {
+        _setTokenURI(tokenId, tokenURI);
+    }
+}
